3c4ca9c13cbff66d4b5bd0f2af578d2f21632166
Peng Du <rick.peng.du@gmail.com>
2020-02-24 18:54:22 -0800

    fixed the bug

--- src/Main.java
+++ src/Main.java
@@ -19,7 +19,7 @@ public class Main {
     public static void bubbleSort(int[] data) {
         for (int i=0; i<data.length; i++) {
             for (int j=0; j<data.length-i-1; j++) {
-                if (data[j]<data[j+1]) {
+                if (data[j]>data[j+1]) {
                     int k = data[j];
                     data[j] = data[j+1];
                     data[j+1] = k;
