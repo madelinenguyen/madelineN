b8e396755e521c92b49e7792f9e7992abd8b2245
Peng Du <rick.peng.du@gmail.com>
2020-02-24 18:55:53 -0800

    refactored swap

--- src/Main.java
+++ src/Main.java
@@ -20,11 +20,15 @@ public class Main {
         for (int i=0; i<data.length; i++) {
             for (int j=0; j<data.length-i-1; j++) {
                 if (data[j]>data[j+1]) {
-                    int k = data[j];
-                    data[j] = data[j+1];
-                    data[j+1] = k;
+                    swap(data, j);
                 }
             }
         }
+    }
+
+    private static void swap(int[] data, int j) {
+        int k = data[j];
+        data[j] = data[j+1];
+        data[j+1] = k;
     }
 }
